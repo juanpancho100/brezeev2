e1d72af0f320a7fb4405f870c5ca5ffd7e9056e7
juanpancho100 <juanpanchoguti@gmail.com>
2021-09-17 11:13:06 -0500

    visualizacion

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <table class="table table-dark table-striped mt-4">
-                        <thead>
+                        <thead>Registro
                         <tr>
                             <th scope="col" class="">ID</th>
                             <th scope="col" class="">Producto</th>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ExpedienteController;
+use App\Models\Expediente;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
